3aa5cbbcfbb23386edd2b7c200317628a6b0dcce
butasun <lunaito39@gmail.com>
2025-01-10 16:44:29 +0900

    Sorry, fix the Minor Issues(Let me know if anything went wrong)

--- BoardScore.py
+++ BoardScore.py
@@ -74,19 +74,19 @@ class BoardScore:
         if(enemy_body[len(enemy_body)-1][0]==1):
             if(my_body[1][1]!=enemy_body[len(enemy_body)-2][1] and my_body[1][1]!=my_body[2][1] ):
                 tmp_map_score[0][enemy_body[len(enemy_body)-1][1]]=-100
-                self.score += map_score[my_body[0][0]][my_body[0][1]]
+                self.score += tmp_map_score[my_body[0][0]][my_body[0][1]]
         if(enemy_body[len(enemy_body)-1][0]==9):
             if(my_body[1][1]!=enemy_body[len(enemy_body)-2][1] and my_body[1][1]!=my_body[2][1] ):
                 tmp_map_score[10][enemy_body[len(enemy_body)-1][1]]=-100
-                self.score += map_score[my_body[0][0]][my_body[0][1]]
+                self.score += tmp_map_score[my_body[0][0]][my_body[0][1]]
         if(enemy_body[len(enemy_body)-1][1]==1):
             if(my_body[1][0]!=enemy_body[len(enemy_body)-2][0] and my_body[1][0]!=my_body[2][0] ):
                 tmp_map_score[enemy_body[len(enemy_body)-1][0]][0]=-100
-                self.score += map_score[my_body[0][0]][my_body[0][1]]
+                self.score += tmp_map_score[my_body[0][0]][my_body[0][1]]
         if(enemy_body[len(enemy_body)-1][1]==9):
             if(my_body[1][0]!=enemy_body[len(enemy_body)-2][0] and my_body[1][0]!=my_body[2][0] ):
                 tmp_map_score[enemy_body[len(enemy_body)-1][0]][10]=-100     
-                self.score += map_score[my_body[0][0]][my_body[0][1]]
+                self.score += tmp_map_score[my_body[0][0]][my_body[0][1]]
 
     def updateScoreByNumOfWays(self, head:tuple[int,int],
                                my_body: list[tuple[int, int]],
